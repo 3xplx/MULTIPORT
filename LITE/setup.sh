d9fc1520e5fd5de6b3c80c8424374d1400e98ec8
3xplx <113743188+3xplx@users.noreply.github.com>
2022-10-11 11:15:58 +0800

    Update setup.sh

--- LITE/setup.sh
+++ LITE/setup.sh
@@ -1,5 +1,64 @@
 #!/bin/bash
 
+# // Make Folder
+rm -rf /root/scvpn_data/ > /dev/null 2>&1
+mkdir -p /root/scvpn_data/ > /dev/null 2>&1
+
+## // Running Update & install Requirement
+apt update -y;
+apt upgrade -y;
+apt autoremove -y;
+apt dist-upgrade -y;
+apt install jq -y;
+apt install wget -y;
+apt install nano -y;
+apt install curl -y;
+
+# // Removing Apache / Nginx if exist
+apt remove --purge nginx -y;
+apt remove --purge apache2 -y;
+apt autoremove -y;
+
+# // Installing Requirement
+apt install net-tools -y;
+apt install netfilter-persistent -y;
+apt install openssl -y;
+apt install iptables -y;
+apt install iptables-persistent -y;
+apt autoremove -y;
+
+# // Installing BBR & FQ
+cat > /etc/sysctl.conf << END
+# Sysctl Config By Nazren Naz @ dotycat.com
+# // Enable IPv4 Forward
+net.ipv4.ip_forward=1
+
+# // Disable IPV6
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+
+# // Enable bbr & fq for optimization
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+END
+sysctl -p;
+
+# // Configuring Socat & Remove nginx & apache if installed
+clear;
+apt install socat -y;
+apt install sudo -y;
+
+# // Stopping Service maybe is installed
+systemctl stop xray-mini@tls > /dev/null 2>&1
+systemctl stop xray-mini@nontls > /dev/null 2>&1
+systemctl stop nginx > /dev/null 2>&1
+systemctl stop apache2 > /dev/null 2>&1
+
+# // Kill port 80 & 443 if already used
+lsof -t -i tcp:80 -s tcp:listen | xargs kill > /dev/null 2>&1
+lsof -t -i tcp:443 -s tcp:listen | xargs kill > /dev/null 2>&1
+
 source /root/scvpn_data/secure.dat;
 
 # // Checking Your Running Or Root or no
